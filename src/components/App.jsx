130a3ccaf1cbc58070f6ca20d62f579621cdff6f
Rogok-Serg <rogoksv@gmail.com>
2023-06-26 00:24:45 +0300

    settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  state = {};
+}
